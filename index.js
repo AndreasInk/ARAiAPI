52687ba2191bdbde359b5579d67b974dfdea9634
Andreas Ink <andreasink17@gmail.com>
2021-06-11 23:22:43 -0400

    a

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const upload = multer({ storage: storage })
 
 app.get('/getImage', function (req, res) {
     
-    res.sendFile("/uploads/uploadedFile.jpg");
+    res.sendFile("/uploads/sampleImage.jpg");
 });
 app.get('/', function(req, res) {
     res.send("Cool! The server is running!")
