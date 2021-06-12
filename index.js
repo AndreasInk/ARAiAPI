4448831969cb34417847d91467364b497d08d357
Andreas Ink <andreasink17@gmail.com>
2021-06-12 01:22:39 -0400

    a

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
       callback(null, file.originalname)
   }
 });
-global.printed = "This can be accessed anywhere!";
+var printed = "This can be accessed anywhere!";
 const upload = multer({ storage: storage })
 
 app.get('/getImage', function (req, res) {
