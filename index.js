e130ccf11e5de27c719d1477d6fe5b4323e2f154
Andreas Ink <andreasink17@gmail.com>
2021-06-12 03:57:49 -0400

    a

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post('/multiupload', upload.array('uploadedFile', 10), (req, res) => {
         console.log("Received files:")
         for (let i=0; i<req.files.length; i++) {
             console.log(req.files[i])
-            ids.push(req.files[i].originalname)
+            ids.push("a")
         }
 
         return res.send({ result: true })
