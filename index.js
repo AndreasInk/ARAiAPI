1413ed51717b09c49aae89a7f06807e57b8fec2c
Andreas Ink <andreasink17@gmail.com>
2021-06-12 05:55:23 -0400

    a

--- index.js
+++ index.js
@@ -23,12 +23,13 @@ app.get('/getIDs', function (req, res) {
 });
 
 app.get('/getImage', function (req, res) {
+    start()
     async function start() {
     res.sendFile(__dirname + "/uploads/" + req.query.id);
     unlinkAsync(__dirname + "/uploads/" + req.query.id)
     ids.shift()
     }
-    start()
+    
 
 });
 app.get('/', function(req, res) {
