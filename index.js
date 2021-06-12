4c468c7ad666981c6cef162c285c74780fdda41f
Andreas Ink <andreasink17@gmail.com>
2021-06-11 23:03:22 -0400

    a

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ const upload = multer({ storage: storage })
 app.get('/', function(req, res) {
     res.send("Cool! The server is running!")
 })
-
+app.get('/uploads', function(req, res) {
+    res.send(storage.files)
+})
 app.post('/upload', upload.single('uploadedFile'), (req, res) => {
     if (req.file) {
         console.log(req.file)
